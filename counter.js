bf5c5bd42304e75b2295146fd402367cd25aab9e
rnpjroz <rnpjrau@gmail.com>
2023-05-04 13:31:29 +1000

    initial checkin

---
+++ counter.js
@@ -0,0 +1,110 @@
+import { LeapHostSdkFactory } from '@leapdev/leap-host';
+
+const sdk = LeapHostSdkFactory.getInstance();
+
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count = ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+
+  if (!!sdk) {
+    sdk.init().then(async () => {     
+      console.log('Hey, Im the matter ID: ' + sdk.leapContext.context.matterId);    
+
+      //openMatter(request: OpenMatterRequest): void;
+
+      // //opens a matter
+      // //Request example:
+      // const request = {
+      //   "matterId": "8c857172-571e-564c-bc5a-7805cf76825c",
+      //   "appSessionId": sdk.leapContext.hostInfo.appSessionId
+      // };
+      
+      // sdk.matter.openMatter(request); 
+      
+      //----------------------------------------------
+      ////opens the card UI, then allows user to create from it. 
+      // sdk.card.createCard().then(() => {
+
+      //    console.log("Card created" + );         
+
+      //});
+
+      // const cardId = (await sdk.card.createCard()).cardId;
+      // console.log("Card created: " + cardId);
+
+      // //-------------------fees: Doesn't work yet
+      // const createFeeEntryRequest = {
+      //   "matterId": "8c857172-571e-564c-bc5a-7805cf76825c",
+      //   "appSessionId": sdk.leapContext.hostInfo.appSessionId,        
+        
+      //   "taskCodeId": "1",
+      //   "taxCodeId": "2",
+      //   "quantity": 105,
+      //   "amountEach": 200,
+      //   "includeTax": false,
+      //   "transactionDate": "05-05-2023",
+      //   "billingDescription": "",
+      //   "billingMode": "1",
+      //   "memo": "This is my memo",
+      //   "staffId": sdk.leapContext.hostInfo.staffId
+      // };      
+
+      // try {
+      //   console.log("Creating fee entry request"); 
+      //   sdk.accounting.createFeeEntry(createFeeEntryRequest);
+      //   console.log("Finished creating fee entry request"); 
+      // }
+      // catch(err) {
+      //   ////document.getElementById("demo").innerHTML = err.message;
+      //   console.log("Error is: " + err.message);
+      // }
+
+      // //----------------------------invoices: Opens a new invoice window, but when I save, it doesn't finish the async call. Ask Andy
+      const invoiceRequest = {
+        "invoiceTo": "John Naismith",
+        "invoiceNumber": "Inv-001-SDKTest",
+        "autoNumber": true,
+        "transactionDate": "4-5-2023",
+        "dueDate": "4-5-2024",
+        "memo": "This is my memo",
+        "status": 1, 
+        "layoutId": ""
+      };      
+
+      const invoiceCreated = (await sdk.accounting.createInvoice(invoiceRequest));
+      
+      if (invoiceCreated) {
+        console.log("Invoice has been  created");
+      }
+      else {
+        console.log("Invoice not created");
+      }      
+
+    });
+  }
+
+}
+
+// import { LeapHostSdkFactory } from '@leapdev/leap-host';
+
+// const sdk = LeapHostSdkFactory.getInstance();
+// const matterIdMessage = ''; 
+
+// if (!!sdk) {
+//   sdk.init().then(async () => { 
+//     console.log('Hey, Im the matterID: ' + leapContext.console.matterId);
+//     matterIdMessage = sdk.leapContext.context.matterId;   
+//   });
+// }
+
+// export function openMatter(element) {
+//   element.addEventListener('click', function(e){
+//     //element.innerHTML = `This is from the OpenMatter JS file`; 
+//     element.innerHTML = 'Matter Id: ' + matterIdMessage; 
+//   })    
+// }
